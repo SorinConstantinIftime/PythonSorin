6f231a6c176b32c1c89c6f911d46326332f91457
unknown <sorin_1314@yahoo.com>
2024-07-09 21:24:20 +0300

    exercitii01

---
+++ 01Intro/hello.py
@@ -0,0 +1,24 @@
+# print("hello python")
+# print(1)
+# print(type(1))
+# print(1.4)
+# print(type(1.4))
+# print(1j)
+# print(type(1j))
+# print(1,type(1))
+# print(1j, type(1j))
+# print(float(1), type(1), type(float(1)))
+# print(5*2)
+# print(7 % 4)
+# # modulo-catul impartirii
+# print(7//4)
+# # restul impartirii
+# print(4**3)
+# ridicare la puterea 3
+# print(1 == 5)
+# print(1 != 5)
+# operator de inegalitate !=
+# print(5 > 3)
+# print(5 <= 3)
+# print(4 < 5 and 4 < 3)
+print("mere" in ['mere', 'mere', 'suc'])
